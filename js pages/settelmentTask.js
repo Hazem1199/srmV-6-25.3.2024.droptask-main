e39ec38eb1d435edfddbfbfcfe71a5a9e4d7db52
Hazem Essam <133036561+Hazem1199@users.noreply.github.com>
2024-03-26 02:02:27 +0200

    26.3.2024

--- js pages/settelmentTask.js
+++ js pages/settelmentTask.js
@@ -19,22 +19,22 @@ async function getTasks() {
   }
 }
 
-async function getreportedTasks() {
-  const baseUrl = `https://srm-vbc7.onrender.com/api/task-reports`;
-  const token =
-    "f2004377863e9d767b12ed40b2a996ff71343b463323b990160adf52f660493e20e77b5f368d4f510a3f9a0ccb3bb2cbed5b8c8a6800c63d768eed032bf0eeeb030cfab84d2167ca498673aeb6528147a103989c27e944e87768be0b2b6c65f5f8ad994a831150e8bce9bbf650261d17cf5f5db8e03182ea2faec183d1ec11de";
-  const headers = {
-    Authorization: `Bearer ${token}`,
-  };
-  try {
-    const response = await fetch(baseUrl, { headers });
-    const data = await response.json();
-    console.log(data.data[0].attributes);
-    return data;
-  } catch (error) {
-    console.error(error);
-  }
-}
+// async function getreportedTasks() {
+//   const baseUrl = `https://srm-vbc7.onrender.com/api/task-reports`;
+//   const token =
+//     "f2004377863e9d767b12ed40b2a996ff71343b463323b990160adf52f660493e20e77b5f368d4f510a3f9a0ccb3bb2cbed5b8c8a6800c63d768eed032bf0eeeb030cfab84d2167ca498673aeb6528147a103989c27e944e87768be0b2b6c65f5f8ad994a831150e8bce9bbf650261d17cf5f5db8e03182ea2faec183d1ec11de";
+//   const headers = {
+//     Authorization: `Bearer ${token}`,
+//   };
+//   try {
+//     const response = await fetch(baseUrl, { headers });
+//     const data = await response.json();
+//     console.log(data.data[0].attributes);
+//     return data;
+//   } catch (error) {
+//     console.error(error);
+//   }
+// }
 
 var overlay = document.createElement("div");
 overlay.style.position = "fixed";
@@ -65,9 +65,9 @@ console.log(taskPush);
 async function displayTasks() {
   change();
 
-  const reportedTasks = await getreportedTasks();
+  // const reportedTasks = await getreportedTasks();
 
-  console.log(reportedTasks.data);
+  // console.log(reportedTasks.data);
 
   const tasks = await getTasks();
 
